b23c42a20e5ae12bc47788de494f2247ccbc854a
Andrés Otero <aotercarb@gmail.com>
2021-01-30 11:19:43 +0100

    refactor: extract ternary operator into function for more clarity

--- src/main/java/gal/andres/vacaloura/utils/ParseUtils.java
+++ src/main/java/gal/andres/vacaloura/utils/ParseUtils.java
@@ -7,6 +7,8 @@ import java.util.stream.Stream;
 
 public class ParseUtils {
 
+  private ParseUtils() {}
+
   /**
    * Build a {@link Sort} object from a list of attributes
    *
@@ -19,12 +21,27 @@ public class ParseUtils {
     return (query != null && !query.isEmpty())
         ? Sort.by(
             Stream.of(query.split(","))
-                .map(
-                    s ->
-                        (s.charAt(0) == '-')
-                            ? Sort.Order.desc(s.substring(1, s.length()))
-                            : Sort.Order.asc(s))
+                .map(ParseUtils::getSortFromParameter)
                 .collect(Collectors.toList()))
         : Sort.by(Sort.Order.desc("id"));
+  }
+
+  /**
+   * Build a Order from a query parameter
+   *
+   * @param parameter {@link String} with a - if the sorting order is descendant
+   * @return {@link org.springframework.data.domain.Sort.Order} of the parameter with the right
+   *     orientation
+   */
+  private static Sort.Order getSortFromParameter(String parameter) {
+    if (isDescendant(parameter)) {
+      return Sort.Order.desc(parameter.substring(1, parameter.length()));
+    } else {
+      return Sort.Order.asc(parameter);
+    }
+  }
+
+  private static boolean isDescendant(String parameter) {
+    return parameter.charAt(0) == '-';
   }
 }
